fe9949f064642ac838c7742a0de452f74dbe86e1
fvergaracl <fvergaracl@gmail.com>
2023-09-26 16:47:30 +0200

    pylint and format some files

--- app/api/core/config.py
+++ app/api/core/config.py
@@ -7,7 +7,12 @@ from typing import List, Union
 from pydantic import AnyHttpUrl, BaseSettings, validator
 import os
 
+
 class Settings(BaseSettings):
+    """
+    Application settings
+    """
+
     SECRET_KEY: str = secrets.token_urlsafe(32)
     # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
@@ -16,12 +21,15 @@ class Settings(BaseSettings):
     MONGODB_DATABASE: str
     COLLECTION_NAME_GAME_SCHEMA: str
     COLLECTION_NAME_GAME_INSTANCE: str
-    
+
     @validator("BACKEND_CORS_ORIGINS", pre=True)
-    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+    def assemble_cors_origins(self, v: Union[str, List[str]]) -> Union[List[str], str]:
+        """
+        Assemble CORS origins
+        """
         if isinstance(v, str) and not v.startswith("["):
             return [i.strip() for i in v.split(",")]
-        elif isinstance(v, (list, str)):
+        if isinstance(v, (list, str)):
             return v
         raise ValueError(v)
 
@@ -29,17 +37,15 @@ class Settings(BaseSettings):
     SERVER_URL: str
     SERVER_METADATA_URL: str
 
-    
-
     KEYCLOAK_CLIENT_ID: str
     KEYCLOAK_URL_REALM: str
-    
 
     PROTOCOL: str
     SERVER_NAME: str
     BASE_PATH: str
-    
-    PRODUCTION_MODE : bool =  "https" in os.getenv("PROTOCOL")
-    PROJECT_NAME : str = "Gamification v2 API"
+
+    PRODUCTION_MODE: bool = "https" in os.getenv("PROTOCOL")
+    PROJECT_NAME: str = "Gamification v2 API"
+
 
 settings = Settings()
--- app/api/db/mongodb.py
+++ app/api/db/mongodb.py
@@ -6,18 +6,25 @@ import os
 MAX_CONNECTIONS_COUNT = int(os.getenv("MAX_CONNECTIONS_COUNT", 10))
 MIN_CONNECTIONS_COUNT = int(os.getenv("MIN_CONNECTIONS_COUNT", 10))
 
+
 class DataBase:
     client: AsyncIOMotorClient = None
+
+
 db = DataBase()
+
 
 async def get_collection(collection_name: str) -> AsyncIOMotorCollection:
     return db.client[settings.MONGODB_DATABASE][collection_name]
 
+
 async def connect_to_mongo():
     logging.info("Connecting to database...")
-    db.client = AsyncIOMotorClient(settings.MONGODB_URL,
-                                   maxPoolSize=MAX_CONNECTIONS_COUNT,
-                                   minPoolSize=MIN_CONNECTIONS_COUNT)
+    db.client = AsyncIOMotorClient(
+        settings.MONGODB_URL,
+        maxPoolSize=MAX_CONNECTIONS_COUNT,
+        minPoolSize=MIN_CONNECTIONS_COUNT,
+    )
     logging.info("Database connected！")
 
 
